b35ffc139799abcdae3d04aa992210fbdcf0c963
Alonso <alonsomartin181006@gmail.com>
2025-02-08 18:50:18 -0800

    Created Finances Interface with some potential methods

---
+++ src/main/model/Finances.java
@@ -0,0 +1,10 @@
+package model;
+
+// Inteface for a user's finances, such as debts and assets.
+public interface Finances {
+    public void getValue();
+    public void getName();
+    public void increaseValue();
+    public void decreaseValue();
+
+}
