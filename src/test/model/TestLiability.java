0b191c7e9c8f1413baca65d436f249b22178acb2
Alonso <alonsomartin181006@gmail.com>
2025-02-10 17:35:10 -0800

    Added testing for Liabilities

--- src/test/model/TestLiability.java
+++ src/test/model/TestLiability.java
@@ -1,5 +1,49 @@
 package model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class TestLiability {
+    private Liability testLiability;
+
+    @BeforeEach
+    void setup(){
+        testLiability = new Liability("student loans", 50.0);
+    }
+
+    @Test
+    public void testConstructor(){
+        assertEquals(50.0, testLiability.getValue());
+        assertEquals("student loans", testLiability.getName());
+    }
+    
+    @Test
+    public void testIncreaseValue(){
+        testLiability.increaseValue(27.5);
+        assertEquals(57.5, testLiability.getValue());
+
+    }
+
+    @Test 
+    public void testDecreaseValue(){
+        testLiability.decreaseValue(17.0);
+        assertEquals(33.0, testLiability.getValue());
+    }
+
+    @Test 
+    public void testIncreaseValueMultipleTimes(){
+        testLiability.increaseValue(12.3);
+        testLiability.increaseValue(17.8);
+        assertEquals(80.1, testLiability.getValue());
+    }
+
+    @Test
+    public void testDecreaseValueMultipleTimes(){
+        testLiability.decreaseValue(0.4);
+        testLiability.decreaseValue(49);
+        assertEquals(0.6, testLiability.getValue());
+    }
 
 }
